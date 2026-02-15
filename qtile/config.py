8135f56d6044f40f5fd0ea19fcaa9d993f6c2466
The-Fool-cmd <calin.sandu.94@gmail.com>
2026-02-15 23:23:33 +0200

    Minor changes to check nix config

--- qtile/config.py
+++ qtile/config.py
@@ -155,7 +155,7 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
-logo = os.path.join(os.path.dirname(libqtile.resources.__file__), "logo.png")
+# logo = os.path.join(os.path.dirname(libqtile.resources.__file__), "logo.png")
 screens = [
     Screen(
         bottom=bar.Bar(
@@ -171,7 +171,6 @@ screens = [
                     name_transform=lambda name: name.upper(),
                 ),
                 # widget.TextBox("default config", name="default"),
-                # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 widget.Systray(),
@@ -179,8 +178,8 @@ screens = [
                 widget.QuickExit(),
             ],
             24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
         #background="#000000",
         #wallpaper=logo,
